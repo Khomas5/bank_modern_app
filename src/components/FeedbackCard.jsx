c4c8dc03597f62fadb95c1bbbc5150bb963c638f
Khomas5 <ana.khomasuridze.2@iliauni.edu.ge>
2023-10-08 20:21:06 +0400

    ready to deploy

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -6,7 +6,7 @@ const FeedbackCard = ({content, name,
   title, img}) => ( 
  <div className='flex justify-between flex-col
  px-10 py-12 rounded-[20px]
- max-w-[370] md:mr-10 sm:mr-5 mr-0 my-5
+ max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5
  feedback-card'>
   <img src={quotes} alt='double_quotes'
   className='w-[42px] h-[27px] object-contain' />
